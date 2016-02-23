337a015eff2549b48f9fb971b1f1201957d40122
FarTeen <934628017@qq.com>
2016-02-23 10:51:30 +0800

    Edit SkipUntil Branch

--- ReactiveCocoaLearning/ViewController.swift
+++ ReactiveCocoaLearning/ViewController.swift
@@ -15,33 +15,32 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         ///skipUntil Example
-//        let (signal, observer) = Signal<String, NoError>.pipe()
-//        let (signal2, observer2) = Signal<(), NoError>.pipe()
-//        let skipUntilSignal = signal.skipUntil(signal2)
-//        
-//        skipUntilSignal.observeNext { (string) -> () in
-//            print("\(NSDate()) skipUntilSignal \(string)")
-//        }
-//        
-//        signal2.observeNext { () -> () in
-//            print("Observer2 has send Next")
-//        }
-//        
-//        dispatch_after(DISPATCH_TIME_NOW, dispatch_get_global_queue(0, 0)) { () -> Void in
-//            QueueScheduler.mainQueueScheduler.scheduleAfter(NSDate(timeIntervalSinceNow: 2), action: { () -> () in
-//                observer.sendNext("timer 22222")
-//            })
-//            QueueScheduler.mainQueueScheduler.scheduleAfter(NSDate(timeIntervalSinceNow: 4), action: { () -> () in
-//                observer.sendNext("timer 44444")
-//            })
-//            QueueScheduler.mainQueueScheduler.scheduleAfter(NSDate(timeIntervalSinceNow: 6), action: { () -> () in
-//                observer.sendNext("timer 66666")
-//            })
-//            QueueScheduler.mainQueueScheduler.scheduleAfter(NSDate(timeIntervalSinceNow: 5), action: { () -> () in
-//                observer2.sendNext(())
-//            })
-//            
-//        }
+        let (signal, observer) = Signal<String, NoError>.pipe()
+        let (signal2, observer2) = Signal<(), NoError>.pipe()
+        let skipUntilSignal = signal.skipUntil(signal2)
+        
+        skipUntilSignal.observeNext { (string) -> () in
+            print("\(NSDate()) skipUntilSignal \(string)")
+        }
+        
+        signal2.observeNext { () -> () in
+            print("Observer2 has send Next")
+        }
+        
+        dispatch_after(DISPATCH_TIME_NOW, dispatch_get_global_queue(0, 0)) { () -> Void in
+            QueueScheduler.mainQueueScheduler.scheduleAfter(NSDate(timeIntervalSinceNow: 2), action: { () -> () in
+                observer.sendNext("timer 22222")
+            })
+            QueueScheduler.mainQueueScheduler.scheduleAfter(NSDate(timeIntervalSinceNow: 4), action: { () -> () in
+                observer.sendNext("timer 44444")
+            })
+            QueueScheduler.mainQueueScheduler.scheduleAfter(NSDate(timeIntervalSinceNow: 6), action: { () -> () in
+                observer.sendNext("timer 66666")
+            })
+            QueueScheduler.mainQueueScheduler.scheduleAfter(NSDate(timeIntervalSinceNow: 5), action: { () -> () in
+                observer2.sendNext(())
+            })
+        }
         
       }
     

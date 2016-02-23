26d9d2762f02e36ccf07048765133cda195803f3
FarTeen <934628017@qq.com>
2016-02-23 14:49:00 +0800

    edit promoteErrors

--- ReactiveCocoaLearning/ViewController.swift
+++ ReactiveCocoaLearning/ViewController.swift
@@ -15,28 +15,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ///timeoutWithError Example
-        let (signal, observer) = Signal<Int, NSError>.pipe()
+        ///promoteErrors Example
+        let (signal, observer) = Signal<Int, NoError>.pipe()
         let (signal2, observer2) = Signal<Int, NSError>.pipe()
         
-        let timeoutWithErrorSignal = signal.timeoutWithError(NSError(domain: "domain 123", code: 1000, userInfo: nil), afterInterval: 10, onScheduler: QueueScheduler.mainQueueScheduler)
+        let promoteErrors = signal.promoteErrors(NSError)
         
-        timeoutWithErrorSignal.observeNext { (next) -> () in
-            print("next \(next)")
-        }
-        timeoutWithErrorSignal.observeFailed { (error) -> () in
-            print(error)
-        }
-        timeoutWithErrorSignal.observeCompleted { () -> () in
-            
-        }
-        
-        observer.sendNext(1)
-        observer.sendNext(2)
-        observer.sendNext(3)
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(10.1 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) { () -> Void in
-            observer.sendCompleted()
-        }
+        let zippedSignal = promoteErrors.zipWith(signal2)
         
         
         

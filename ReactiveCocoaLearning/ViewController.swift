a2dcc8611fee16406ea127cc2c7f2ce512f96baa
FarTeen <934628017@qq.com>
2016-02-23 14:35:04 +0800

    edit combineLatest

--- ReactiveCocoaLearning/ViewController.swift
+++ ReactiveCocoaLearning/ViewController.swift
@@ -15,44 +15,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ///throttle Example
-        ///1.结果?
-        ///2.顺序?
-        ///3.结论什么是throttle
+        ///combineLatest Example
         let (signal, observer) = Signal<Int, NSError>.pipe()
-        let (signal2, observer2) = Signal<Int, NoError>.pipe()
+        let (signal2, observer2) = Signal<Int, NSError>.pipe()
         
-        let throttleSignal = signal.throttle(5.1, onScheduler: QueueScheduler.mainQueueScheduler)
-        throttleSignal.observeNext { (next) -> () in
-            print("\(NSDate()) throttle \(next)")
+        let combineLatest = signal.combineLatestWith(signal2)
+        
+        combineLatest.observeNext { (next1, next2) -> () in
+            print("next1 \(next1) next2 \(next2)")
         }
         
         observer.sendNext(1)
         observer.sendNext(2)
         observer.sendNext(3)
+        observer2.sendNext(9)
+        observer2.sendNext(8)
         
-        print("\(NSDate())")
-        
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(2.0 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) { () -> Void in
-            observer.sendNext(1000)
-        }
-        
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(5.2 * Double(NSEC_PER_SEC))), dispatch_get_global_queue(0, 0)) { () -> Void in
-            observer.sendNext(4)
-            observer.sendNext(5)
-            dispatch_after(DISPATCH_TIME_NOW, dispatch_get_global_queue(0, 0), { () -> Void in
-                observer.sendNext(6)
-            })
-        }
-
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(6.0 * Double(NSEC_PER_SEC))), dispatch_get_global_queue(0, 0)) { () -> Void in
-            observer.sendNext(7)
-            
-        }
-        
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(11.0 * Double(NSEC_PER_SEC))), dispatch_get_global_queue(0, 0)) { () -> Void in
-            observer.sendNext(8)
-        }
         
         
         

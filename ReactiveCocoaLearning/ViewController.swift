c9f2087b731cd4cc5c5bcfb3faf7673e357fa872
FarTeen <934628017@qq.com>
2016-02-23 15:36:08 +0800

    edit Collect

--- ReactiveCocoaLearning/ViewController.swift
+++ ReactiveCocoaLearning/ViewController.swift
@@ -15,18 +15,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ///observeOn Example
+        ///collect Example
         let (signal, observer) = Signal<String, NoError>.pipe()
         let (signal2, observer2) = Signal<String, NoError>.pipe()
-        let observeOnSignal = signal.observeOn(ImmediateScheduler.init())//QueueScheduler.init(queue: dispatch_get_global_queue(0, 0)))
-        observeOnSignal.observeNext { (next) -> () in
-            print("current  \(NSThread.currentThread())")
-            print("main     \(NSThread.mainThread())")
+        let collectSignal = signal.collect()
+        collectSignal.observeNext { (next) -> () in
             print(next)
+        }
+        collectSignal.observeCompleted { () -> () in
+            
         }
         
         observer.sendNext("1")
         observer.sendNext("2")
+        observer.sendNext("3")
+//        observer.sendNext(nil)
+        observer.sendCompleted()
         
         
       }

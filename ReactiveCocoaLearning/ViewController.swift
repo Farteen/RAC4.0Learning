80a05b52aa657f4b71f4981da62157e6b68ef5cd
FarTeen <934628017@qq.com>
2016-02-23 14:43:58 +0800

    edit timeoutWithError

--- ReactiveCocoaLearning/ViewController.swift
+++ ReactiveCocoaLearning/ViewController.swift
@@ -15,22 +15,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ///combineLatest Example
+        ///timeoutWithError Example
         let (signal, observer) = Signal<Int, NSError>.pipe()
         let (signal2, observer2) = Signal<Int, NSError>.pipe()
         
-        let combineLatest = signal.combineLatestWith(signal2)
+        let timeoutWithErrorSignal = signal.timeoutWithError(NSError(domain: "domain 123", code: 1000, userInfo: nil), afterInterval: 10, onScheduler: QueueScheduler.mainQueueScheduler)
         
-        combineLatest.observeNext { (next1, next2) -> () in
-            print("next1 \(next1) next2 \(next2)")
+        timeoutWithErrorSignal.observeNext { (next) -> () in
+            print("next \(next)")
+        }
+        timeoutWithErrorSignal.observeFailed { (error) -> () in
+            print(error)
+        }
+        timeoutWithErrorSignal.observeCompleted { () -> () in
+            
         }
         
         observer.sendNext(1)
         observer.sendNext(2)
         observer.sendNext(3)
-        observer2.sendNext(9)
-        observer2.sendNext(8)
-        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(10.1 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) { () -> Void in
+            observer.sendCompleted()
+        }
         
         
         

9f281a5a9801242671ec71734991394a6f948f83
FarTeen <934628017@qq.com>
2016-02-25 14:23:32 +0800

    edit SignalProducer

--- ReactiveCocoaLearning/ViewController.swift
+++ ReactiveCocoaLearning/ViewController.swift
@@ -10,37 +10,87 @@ import UIKit
 import ReactiveCocoa
 import Result
 
+//From https://gist.github.com/natecook1000/b0285b518576b22c4dc8
+extension NSTimer {
+    /**
+     Creates and schedules a one-time `NSTimer` instance.
+     
+     - Parameters:
+     - delay: The delay before execution.
+     - handler: A closure to execute after `delay`.
+     
+     - Returns: The newly-created `NSTimer` instance.
+     */
+    class func schedule(delay delay: NSTimeInterval, handler: NSTimer! -> Void) -> NSTimer {
+        let fireDate = delay + CFAbsoluteTimeGetCurrent()
+        let timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, fireDate, 0, 0, 0, handler)
+        CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, kCFRunLoopCommonModes)
+        return timer
+    }
+    
+    /**
+     Creates and schedules a repeating `NSTimer` instance.
+     
+     - Parameters:
+     - repeatInterval: The interval (in seconds) between each execution of
+     `handler`. Note that individual calls may be delayed; subsequent calls
+     to `handler` will be based on the time the timer was created.
+     - handler: A closure to execute at each `repeatInterval`.
+     
+     - Returns: The newly-created `NSTimer` instance.
+     */
+    class func schedule(repeatInterval interval: NSTimeInterval, handler: NSTimer! -> Void) -> NSTimer {
+        let fireDate = interval + CFAbsoluteTimeGetCurrent()
+        let timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, fireDate, interval, 0, 0, handler)
+        CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, kCFRunLoopCommonModes)
+        return timer
+    }
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var tfInput: UITextField!
     
+    func timerAction(timer: NSTimer) {
+        let observer = timer.userInfo as! Observer<Int, NoError>
+        observer.sendNext(123)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        /// Example
-        let (signal, observer) = Signal<Int, NSError>.pipe()
-        signal.observe { (event) -> () in
-            switch event {
-            case let .Next(next):
-                print("next ----    \(next)")
-            case let .Failed(error):
-                print("error \(error)")
-            default:
-                print(event)
-            }
+        ///InjectingEffects Example
+        let signalProducer = SignalProducer<Int, NoError> { observer, disposal in
+            NSTimer.schedule(repeatInterval: 2.0, handler: { (timer) -> Void in
+                observer.sendNext(123)
+            })
         }
         
-        signal.observeNext { (next) -> () in
-            print("next \(next)")
-        }
-        signal.observeFailed { (error) -> () in
-            print("error \(error)")
+        signalProducer.startWithNext { (next) -> () in
+            print(next)
         }
         
-        observer.sendNext(1)
-        observer.sendNext(2)
-        observer.sendFailed(NSError(domain: "com.apple.com", code: 1000, userInfo: nil))
-        observer.sendCompleted()
+//        let resultSignalProducer = signalProducer.on(started: { () -> () in
+//                print("started")
+//            }, event: { (event) -> () in
+//                
+//                print("event")
+//            }, failed: { (error) -> () in
+//                print("failed")
+//            }, completed: { () -> () in
+//                print("completed")
+//            }, interrupted: { () -> () in
+//                print("interupted")
+//            }, terminated: { () -> () in
+//                print("terminated")
+//            }, disposed: { () -> () in
+//                print("disposed")
+//            }) { (next) -> () in
+//                
+//                print("happpend \(next)")
+//        }
+//        
+//        cansendObserver.sendNext(123123)
         
       }
     
